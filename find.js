d6d63310765a6cdca496fd6864d7d125f875466e
ankithanaredla22-beep <ankithanaredla22@gmail.com>
2026-02-26 20:48:21 +0530

    Add files via upload

--- find.js
+++ find.js
@@ -1,96 +1,156 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _createTester = require('./internal/createTester.js');
-
-var _createTester2 = _interopRequireDefault(_createTester);
-
-var _eachOf = require('./eachOf.js');
-
-var _eachOf2 = _interopRequireDefault(_eachOf);
-
-var _awaitify = require('./internal/awaitify.js');
-
-var _awaitify2 = _interopRequireDefault(_awaitify);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/**
- * Returns the first value in `coll` that passes an async truth test. The
- * `iteratee` is applied in parallel, meaning the first iteratee to return
- * `true` will fire the detect `callback` with that result. That means the
- * result might not be the first item in the original `coll` (in terms of order)
- * that passes the test.
-
- * If order within the original `coll` is important, then look at
- * [`detectSeries`]{@link module:Collections.detectSeries}.
- *
- * @name detect
- * @static
- * @memberOf module:Collections
- * @method
- * @alias find
- * @category Collections
- * @param {Array|Iterable|AsyncIterable|Object} coll - A collection to iterate over.
- * @param {AsyncFunction} iteratee - A truth test to apply to each item in `coll`.
- * The iteratee must complete with a boolean value as its result.
- * Invoked with (item, callback).
- * @param {Function} [callback] - A callback which is called as soon as any
- * iteratee returns `true`, or after all the `iteratee` functions have finished.
- * Result will be the first item in the array that passes the truth test
- * (iteratee) or the value `undefined` if none passed. Invoked with
- * (err, result).
- * @returns {Promise} a promise, if a callback is omitted
- * @example
- *
- * // dir1 is a directory that contains file1.txt, file2.txt
- * // dir2 is a directory that contains file3.txt, file4.txt
- * // dir3 is a directory that contains file5.txt
- *
- * // asynchronous function that checks if a file exists
- * function fileExists(file, callback) {
- *    fs.access(file, fs.constants.F_OK, (err) => {
- *        callback(null, !err);
- *    });
- * }
- *
- * async.detect(['file3.txt','file2.txt','dir1/file1.txt'], fileExists,
- *    function(err, result) {
- *        console.log(result);
- *        // dir1/file1.txt
- *        // result now equals the first file in the list that exists
- *    }
- *);
- *
- * // Using Promises
- * async.detect(['file3.txt','file2.txt','dir1/file1.txt'], fileExists)
- * .then(result => {
- *     console.log(result);
- *     // dir1/file1.txt
- *     // result now equals the first file in the list that exists
- * }).catch(err => {
- *     console.log(err);
- * });
- *
- * // Using async/await
- * async () => {
- *     try {
- *         let result = await async.detect(['file3.txt','file2.txt','dir1/file1.txt'], fileExists);
- *         console.log(result);
- *         // dir1/file1.txt
- *         // result now equals the file in the list that exists
- *     }
- *     catch (err) {
- *         console.log(err);
- *     }
- * }
- *
- */
-function detect(coll, iteratee, callback) {
-    return (0, _createTester2.default)(bool => bool, (res, item) => item)(_eachOf2.default, coll, iteratee, callback);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FindOperation = void 0;
+const responses_1 = require("../cmap/wire_protocol/responses");
+const error_1 = require("../error");
+const explain_1 = require("../explain");
+const read_concern_1 = require("../read_concern");
+const sort_1 = require("../sort");
+const utils_1 = require("../utils");
+const command_1 = require("./command");
+const operation_1 = require("./operation");
+/** @internal */
+class FindOperation extends command_1.CommandOperation {
+    constructor(ns, filter = {}, options = {}) {
+        super(undefined, options);
+        this.options = { ...options };
+        delete this.options.writeConcern;
+        this.ns = ns;
+        if (typeof filter !== 'object' || Array.isArray(filter)) {
+            throw new error_1.MongoInvalidArgumentError('Query filter must be a plain object or ObjectId');
+        }
+        // special case passing in an ObjectId as a filter
+        this.filter = filter != null && filter._bsontype === 'ObjectId' ? { _id: filter } : filter;
+    }
+    get commandName() {
+        return 'find';
+    }
+    async execute(server, session, timeoutContext) {
+        this.server = server;
+        const options = this.options;
+        let findCommand = makeFindCommand(this.ns, this.filter, options);
+        if (this.explain) {
+            (0, explain_1.validateExplainTimeoutOptions)(this.options, this.explain);
+            findCommand = (0, explain_1.decorateWithExplain)(findCommand, this.explain);
+        }
+        return await server.command(this.ns, findCommand, {
+            ...this.options,
+            ...this.bsonOptions,
+            documentsReturnedIn: 'firstBatch',
+            session,
+            timeoutContext
+        }, this.explain ? responses_1.ExplainedCursorResponse : responses_1.CursorResponse);
+    }
 }
-exports.default = (0, _awaitify2.default)(detect, 3);
-module.exports = exports.default;
+exports.FindOperation = FindOperation;
+function makeFindCommand(ns, filter, options) {
+    const findCommand = {
+        find: ns.collection,
+        filter
+    };
+    if (options.sort) {
+        findCommand.sort = (0, sort_1.formatSort)(options.sort);
+    }
+    if (options.projection) {
+        let projection = options.projection;
+        if (projection && Array.isArray(projection)) {
+            projection = projection.length
+                ? projection.reduce((result, field) => {
+                    result[field] = 1;
+                    return result;
+                }, {})
+                : { _id: 1 };
+        }
+        findCommand.projection = projection;
+    }
+    if (options.hint) {
+        findCommand.hint = (0, utils_1.normalizeHintField)(options.hint);
+    }
+    if (typeof options.skip === 'number') {
+        findCommand.skip = options.skip;
+    }
+    if (typeof options.limit === 'number') {
+        if (options.limit < 0) {
+            findCommand.limit = -options.limit;
+            findCommand.singleBatch = true;
+        }
+        else {
+            findCommand.limit = options.limit;
+        }
+    }
+    if (typeof options.batchSize === 'number') {
+        if (options.batchSize < 0) {
+            if (options.limit &&
+                options.limit !== 0 &&
+                Math.abs(options.batchSize) < Math.abs(options.limit)) {
+                findCommand.limit = -options.batchSize;
+            }
+            findCommand.singleBatch = true;
+        }
+        else {
+            findCommand.batchSize = options.batchSize;
+        }
+    }
+    if (typeof options.singleBatch === 'boolean') {
+        findCommand.singleBatch = options.singleBatch;
+    }
+    // we check for undefined specifically here to allow falsy values
+    // eslint-disable-next-line no-restricted-syntax
+    if (options.comment !== undefined) {
+        findCommand.comment = options.comment;
+    }
+    if (typeof options.maxTimeMS === 'number') {
+        findCommand.maxTimeMS = options.maxTimeMS;
+    }
+    const readConcern = read_concern_1.ReadConcern.fromOptions(options);
+    if (readConcern) {
+        findCommand.readConcern = readConcern.toJSON();
+    }
+    if (options.max) {
+        findCommand.max = options.max;
+    }
+    if (options.min) {
+        findCommand.min = options.min;
+    }
+    if (typeof options.returnKey === 'boolean') {
+        findCommand.returnKey = options.returnKey;
+    }
+    if (typeof options.showRecordId === 'boolean') {
+        findCommand.showRecordId = options.showRecordId;
+    }
+    if (typeof options.tailable === 'boolean') {
+        findCommand.tailable = options.tailable;
+    }
+    if (typeof options.oplogReplay === 'boolean') {
+        findCommand.oplogReplay = options.oplogReplay;
+    }
+    if (typeof options.timeout === 'boolean') {
+        findCommand.noCursorTimeout = !options.timeout;
+    }
+    else if (typeof options.noCursorTimeout === 'boolean') {
+        findCommand.noCursorTimeout = options.noCursorTimeout;
+    }
+    if (typeof options.awaitData === 'boolean') {
+        findCommand.awaitData = options.awaitData;
+    }
+    if (typeof options.allowPartialResults === 'boolean') {
+        findCommand.allowPartialResults = options.allowPartialResults;
+    }
+    if (options.collation) {
+        findCommand.collation = options.collation;
+    }
+    if (typeof options.allowDiskUse === 'boolean') {
+        findCommand.allowDiskUse = options.allowDiskUse;
+    }
+    if (options.let) {
+        findCommand.let = options.let;
+    }
+    return findCommand;
+}
+(0, operation_1.defineAspects)(FindOperation, [
+    operation_1.Aspect.READ_OPERATION,
+    operation_1.Aspect.RETRYABLE,
+    operation_1.Aspect.EXPLAINABLE,
+    operation_1.Aspect.CURSOR_CREATING
+]);
+//# sourceMappingURL=find.js.map
